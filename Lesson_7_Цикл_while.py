ca7cf8e2276440a4083c20b2570994ca543d06a2
Sarmat <Spirator@Yandex.ru>
2023-08-20 11:57:23 +0700

    initial commit

---
+++ Lesson_7_Цикл_while.py
@@ -0,0 +1,20 @@
+# Цикл while
+# после каждого ввода сайта, программа будет запрашивать ввод сайта.
+
+import os
+
+while True:
+    website = input("Введите адрес сайта\n ")
+    if website == "Завершить":
+        break
+        if "https://" in website:
+            os.system("start" + website)
+            print("if")
+        elif "www." in website:
+            website = "https://" + website
+            os.system("start" + website)
+            print("elif")
+        else:
+            website = "https:// www" + website
+            os.system("start" + website)
+            print("else")
