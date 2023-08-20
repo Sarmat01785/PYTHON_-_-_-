5548131f301fa0f32aa880f74884cd9896b68e3a
Sarmat <Spirator@Yandex.ru>
2023-08-20 11:30:04 +0700

    initial commit

---
+++ Lesson_3_Условные_операторы.py
@@ -0,0 +1,58 @@
+# Условные операторы if, elif, else
+# if True:
+#     print("if")
+# elif True:
+#     print("True")
+# else:
+#     print("else")
+
+# if False:
+#     print("if")
+# elif True:
+#     print("True")
+# else:
+#     print("else")
+
+# if False:
+#     print("if")
+# elif False:
+#     print("True")
+# else:
+#     print("else")
+
+
+# x = 0
+# if x == 0:
+#     print("if")
+# elif x > 0:
+#     print("True")
+# else:
+#     print("else")
+
+
+# x = 3
+# if x == 0:
+#     print("if")
+# elif x > 0:
+#     print("True")
+# else:
+#     print("else")
+
+# x = -5
+# if x == 0:
+#     print("if")
+# elif x > 0:
+#     print("True")
+# else:
+#     print("else")
+
+# x = 5
+# if x == 0:
+#     x += 1
+# print(5 / x)
+
+
+x = 0
+if x == 0:
+    x += 1
+print(5 / x)
