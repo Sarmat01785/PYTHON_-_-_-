9312543476073c2f9f720af3d0a13c2d4096858d
Sarmat <Spirator@Yandex.ru>
2023-08-20 12:29:53 +0700

    initial commit

---
+++ Lesson_14_Функции_def_определение_и_вызов.py
@@ -0,0 +1,79 @@
+# Функции def, определение и вызов
+# print("До функции")
+#
+#
+# def show():
+#     print("Функция")
+#
+#
+# print("После функции")
+
+# print("До функции")
+#
+#
+# def show():
+#     print("Функция")
+#
+#
+# show()
+#
+# print("После функции")
+
+# print("До функции")
+#
+#
+# def show():
+#     print("Функция")
+#
+#
+# show()
+#
+# print("После функции")
+# show()
+
+
+# def show():
+#     print("Функция")
+#
+#
+# def show2():
+#     x = 7
+#     return x
+#
+#
+# y = show2()
+# print(y)
+#
+# show()
+
+
+# def show():
+#     print("Функция")
+#
+#
+# def show2():
+#     x = 7
+#     return x
+#
+#
+# y = show2()
+# z = show2() + 9
+# print(z)
+#
+# print(show())
+
+def show():
+    print("Функция")
+
+
+def show2():
+    x = 7 + z
+    return x
+
+
+z = 7
+
+y = show2()
+print(y)
+z = 9
+print(show2())
