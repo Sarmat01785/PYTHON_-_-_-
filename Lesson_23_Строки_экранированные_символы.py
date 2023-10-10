49081661848f008320c4491d352f8792f200a243
Sarmat <Spirator@Yandex.ru>
2023-10-10 17:55:30 +0700

    initial commit

---
+++ Lesson_23_Строки_экранированные_символы.py
@@ -0,0 +1,72 @@
+# Строки, экранированные символы
+
+# s = "Lets write a string as 's'"
+
+# print(s) # Lets write a string as 's'
+
+
+# s = 'Let\'s write a string as "s"'
+
+# print(s) # Let's write a string as "s"
+
+
+# s = "Lets \
+#     write a \
+#         string as s"
+# print(s) # Lets     write a         string as s
+
+
+# s = "Lets write\ a string as s"
+
+# print(s)  # Lets write\ a string as s
+
+
+# s = 'Lets write a stri\ng as s'
+
+# print(s)  # Lets write a stri
+#           # g as s
+
+
+# s = 'Lets write a stri\nng as s'
+
+# print(s)  # Lets write a stri
+#           # ng as s
+
+
+# s = 'Let\'s write a string as s'
+
+# print(s)  # Let's write a string as s
+
+
+# s = "Let's write a string as s"
+
+# url = "https:\\www.youtube.com"
+
+# print(url)  # https:\www.youtube.com
+
+
+# s = "Let's write a string as s"
+
+# url = "https:\\www.youtube.com\\new"
+
+# print(url)  # https:\www.youtube.com\new
+
+
+# import os
+# s = "Let's write a string as s"
+
+# url = r"https:\\www.youtube.com\new"
+
+# # os.walk("D:\\")
+
+# print(url)  # https:\www.youtube.com\new
+
+
+s = "Let's write a string as s"
+
+url = "https:\\www.youtube.com\\new"
+
+x = 'C:\\Users\\PyHS\\Desktop'
+
+print(x) # C:\Users\PyHS\Desktop
+print(url)  # https:\www.youtube.com\new
