a4c20fe88475fde38c7f562656d7ad679c9b80a0
Sarmat <Spirator@Yandex.ru>
2023-11-02 21:01:36 +0700

    initial commit

---
+++ Lesson_28_Импорт_модулей.py
@@ -0,0 +1,14 @@
+# Импорт модулей, if _name_ == '__main__''
+
+
+# x =7
+# print(dir())  # ['__annotations__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '
+# # __package__', '__spec__', 'x']
+
+# print(help('__annotations__'))
+
+# import sys
+
+# print(sys.path)
+
+
