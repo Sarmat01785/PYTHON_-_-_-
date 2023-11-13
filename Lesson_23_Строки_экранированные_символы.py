016f051a5a384e637525d6ce052a271c3d50c0fe
Sarmat <Spirator@Yandex.ru>
2023-11-13 16:38:54 +0700

    Made additions

--- Lesson_23_Строки_экранированные_символы.py
+++ Lesson_23_Строки_экранированные_символы.py
@@ -1,4 +1,40 @@
 # Строки, экранированные символы
+'''
+Строки в языке программирования Python используются для хранения текстовых данных. 
+Когда в строке требуется использовать специальные символы, такие как кавычки или перенос строки, 
+нужно использовать экранированные символы. Экранированный символ обозначается 
+обратной косой чертой (\\) перед особым символом.
+'''
+
+# Вот несколько примеров экранированных символов:
+
+# 1. Экранирование кавычек:
+string_with_quotes = "I\'m a string with quotes"
+print(string_with_quotes) # I'm a string with quotes
+
+
+# 2. Экранирование обратной косой черты:
+string_with_backslash = "This is a backslash: \\"
+print(string_with_backslash) # This is a backslash: \
+
+
+# 3. Экранирование переноса строки:
+multi_line_string = "This is a multi-line string.\nHere is the second line."
+print(multi_line_string) # This is a multi-line string.
+                         # Here is the second line.
+
+
+# 4. Экранирование символа табуляции:
+tabbed_string = "This is a tabbed string.\tHere is some tabbed text."
+print(tabbed_string) # This is a tabbed string.    Here is some tabbed text.
+
+
+
+
+
+
+
+
 
 # s = "Lets write a string as 's'"
 
@@ -62,11 +98,11 @@
 # print(url)  # https:\www.youtube.com\new
 
 
-s = "Let's write a string as s"
+# s = "Let's write a string as s"
 
-url = "https:\\www.youtube.com\\new"
+# url = "https:\\www.youtube.com\\new"
 
-x = 'C:\\Users\\PyHS\\Desktop'
+# x = 'C:\\Users\\PyHS\\Desktop'
 
-print(x) # C:\Users\PyHS\Desktop
-print(url)  # https:\www.youtube.com\new
+# print(x) # C:\Users\PyHS\Desktop
+# print(url)  # https:\www.youtube.com\new
