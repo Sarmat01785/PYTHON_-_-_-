159094079c7bae6b3eed7d056563662dc1f33e69
Sarmat <Spirator@Yandex.ru>
2024-04-01 22:00:23 +0700

    Внесены большие изменения

--- Lesson_13_Практика_python_модуль_os_функция_walk.py
+++ Lesson_13_Практика_python_модуль_os_функция_walk.py
@@ -1,54 +1,33 @@
-# Практика python, модуль os, функция walk - (позволяет сгенерировать пути ко
-# всем файлам на компьютере.)
-
-
-# import os
-#
-# for i in os.walk(r"C:\Users\Сармат\Documents\Обучение"):
-#     print(i)
-#     break
-
-
-# import os
-#
-# for i in os.walk(r"C:\Users\Сармат\Documents\Обучение"):
-#     print(i)
-
-# import os
-#
-# list = []
-# for address, dirs, files in os.walk(r"C:\Users\Сармат\Pictures"):
-#     list.append(address)
-# print(list)
-
-
-# import os
-#
-# list = []
-# for address, dirs, files in os.walk(r"C:\Users\Сармат\Pictures"):
-#     for file in files:
-#         list.append(os.path.join(address, file))
-# print(list)
-
-
-# import os
-#
-# list = []
-# for address, dirs, files in os.walk(r"C:\Users\Сармат\Pictures"):
-#     for file in files:
-#         full = os.path.join(address, file)
-#         if "jpg" in full:
-#             list.append(full)
-# print(list)
-
+# Практика Python: использование модуля os и функции walk
 
 import os
 import time
 
-list = []
-for address, dirs, files in os.walk(r"C:\Users\Сармат\Pictures"):
-    for file in files:
-        full = os.path.join(address, file)
-        if time.time() - os.path.getctime(full) < 86400:
-            list.append(full)
-print(list)
+
+# Функция для получения списка всех файлов в каталоге за последние 24 часа
+def get_recent_files(directory, timeframe=86400):
+    recent_files = []
+    for address, dirs, files in os.walk(directory):
+        for file in files:
+            full_path = os.path.abspath(os.path.join(address, file))
+            # Проверяем, что файл был создан в течение последних 24 часов
+            if time.time() - os.path.getctime(full_path) < timeframe:
+                recent_files.append(full_path)
+    return recent_files
+
+
+# Замените "C:\Users\Сармат\Pictures" на путь к вашей целевой папке
+files = get_recent_files(r"C:\Users\Сармат\Pictures")
+print(files)
+
+
+# Функция для получения списка файлов с определенным расширением
+def get_files_with_extension(directory, extension):
+    return [os.path.join(address, file)
+            for address, dirs, files in os.walk(directory)
+            for file in files if file.lower().endswith(f".{extension.lower()}")]
+
+
+# Получаем список файлов JPG
+jpg_files = get_files_with_extension(r"C:\Users\Сармат\Pictures", 'jpg')
+print(jpg_files)
