981bd504417de338c1fb5706e9743b31bb5e8f88
Sarmat <Spirator@Yandex.ru>
2024-04-06 11:49:15 +0700

    Внесены большие изменения

--- Lesson_23_Строки_экранированные_символы.py
+++ Lesson_23_Строки_экранированные_символы.py
@@ -1,108 +1,40 @@
-# Строки, экранированные символы
-'''
-Строки в языке программирования Python используются для хранения текстовых данных. 
-Когда в строке требуется использовать специальные символы, такие как кавычки или перенос строки, 
-нужно использовать экранированные символы. Экранированный символ обозначается 
-обратной косой чертой (\\) перед особым символом.
-'''
+# Строки и экранированные символы в Python
 
-# Вот несколько примеров экранированных символов:
-
-# 1. Экранирование кавычек:
+# Экранирование кавычек позволяет включать кавычки в строку
 string_with_quotes = "I\'m a string with quotes"
-print(string_with_quotes) # I'm a string with quotes
+print(string_with_quotes)  # Вывод: I'm a string with quotes
 
-
-# 2. Экранирование обратной косой черты:
+# Экранирование обратной косой черты позволяет включить сам символ обратной косой черты в строку
 string_with_backslash = "This is a backslash: \\"
-print(string_with_backslash) # This is a backslash: \
+print(string_with_backslash)  # Вывод: This is a backslash: \
 
-
-# 3. Экранирование переноса строки:
+# Экранирование переноса строки создает многострочный текст в одной строке
 multi_line_string = "This is a multi-line string.\nHere is the second line."
-print(multi_line_string) # This is a multi-line string.
-                         # Here is the second line.
+print(multi_line_string)
+# Вывод:
+# This is a multi-line string.
+# Here is the second line.
 
-
-# 4. Экранирование символа табуляции:
+# Экранирование символа табуляции вставляет табуляцию в строку
 tabbed_string = "This is a tabbed string.\tHere is some tabbed text."
-print(tabbed_string) # This is a tabbed string.    Here is some tabbed text.
+print(tabbed_string)  # Вывод: This is a tabbed string.    Here is some tabbed text.
 
+# Использование одинарных и двойных кавычек для включения кавычек в строку без экранирования
+s = "Let's write a string as 's'"
+print(s)  # Вывод: Let's write a string as 's'
 
+s = 'Let\'s write a string as "s"'
+print(s)  # Вывод: Let's write a string as "s"
 
+# Raw-строки используются для предотвращения экранирования специальных символов
+# Особенно полезны при работе с регулярными выражениями или путями файлов
+url = r"https:\\www.youtube.com\new"
+print(url)  # Вывод: https:\\www.youtube.com\new
 
+# Пути к файлам в Windows лучше представлять с помощью raw-строк
+path = r'C:\Users\PyHS\Desktop'
+print(path)  # Вывод: C:\Users\PyHS\Desktop
 
-
-
-
-
-# s = "Lets write a string as 's'"
-
-# print(s) # Lets write a string as 's'
-
-
-# s = 'Let\'s write a string as "s"'
-
-# print(s) # Let's write a string as "s"
-
-
-# s = "Lets \
-#     write a \
-#         string as s"
-# print(s) # Lets     write a         string as s
-
-
-# s = "Lets write\ a string as s"
-
-# print(s)  # Lets write\ a string as s
-
-
-# s = 'Lets write a stri\ng as s'
-
-# print(s)  # Lets write a stri
-#           # g as s
-
-
-# s = 'Lets write a stri\nng as s'
-
-# print(s)  # Lets write a stri
-#           # ng as s
-
-
-# s = 'Let\'s write a string as s'
-
-# print(s)  # Let's write a string as s
-
-
-# s = "Let's write a string as s"
-
-# url = "https:\\www.youtube.com"
-
-# print(url)  # https:\www.youtube.com
-
-
-# s = "Let's write a string as s"
-
-# url = "https:\\www.youtube.com\\new"
-
-# print(url)  # https:\www.youtube.com\new
-
-
-# import os
-# s = "Let's write a string as s"
-
-# url = r"https:\\www.youtube.com\new"
-
-# # os.walk("D:\\")
-
-# print(url)  # https:\www.youtube.com\new
-
-
-# s = "Let's write a string as s"
-
-# url = "https:\\www.youtube.com\\new"
-
-# x = 'C:\\Users\\PyHS\\Desktop'
-
-# print(x) # C:\Users\PyHS\Desktop
-# print(url)  # https:\www.youtube.com\new
+# URL лучше представлять с использованием прямых слешей
+url = "https://www.youtube.com/new"
+print(url)  # Вывод: https://www.youtube.com/new
