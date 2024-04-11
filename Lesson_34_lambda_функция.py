11b63b194444b347635e6f69ef76ea0baeaa763d
Sarmat <Spirator@Yandex.ru>
2024-04-11 21:27:03 +0700

    Внесены большие изменения

--- Lesson_34_lambda_функция.py
+++ Lesson_34_lambda_функция.py
@@ -1,72 +1,49 @@
-#  lambda функция
-"""
-Lambda-функция в Python - это анонимная (безымянная) функция, которая может быть определена 
-без использования ключевого слова def. Она используется для создания простых функций, 
-которые могут быть переданы в качестве аргументов или возвращены из других функций.
-"""
-# Простой синтаксис lambda-функции:
+# Lambda-функции в Python
+
+# Lambda-функция - это компактный способ создания анонимных функций, то есть функций без имени.
+# Они обычно используются для однократных операций, когда необходима небольшая функция.
+
+# Синтаксис lambda-функции:
 # lambda arguments: expression
-# Где arguments - список аргументов функции,
-# а expression - выражение, которое будет выполнено и возвращено.
+# Здесь `arguments` - это аргументы, как в обычной функции, а `expression` - выражение,
+# значение которого lambda-функция возвращает.
 
+# Примеры использования lambda-функций:
 
-# 1. Пример использования lambda-функции для сложения двух чисел:
+# Сложение двух чисел:
 add = lambda x, y: x + y
-result = add(5, 3)
-print(result)  # Вывод: 8
+print(add(5, 3))  # Вывод: 8
 
-
-# 2. Пример использования lambda-функции для проверки, является ли число четным:
+# Проверка числа на четность:
 is_even = lambda num: num % 2 == 0
 print(is_even(4))  # Вывод: True
 print(is_even(7))  # Вывод: False
 
-
-# 3. Пример использования lambda-функции вместе с функцией map() для применения функции к каждому элементу списка:
+# Использование lambda с map для возведения в квадрат всех чисел списка:
 numbers = [1, 2, 3, 4, 5]
-squared_numbers = list(map(lambda x: x**2, numbers))
+squared_numbers = list(map(lambda x: x ** 2, numbers))
 print(squared_numbers)  # Вывод: [1, 4, 9, 16, 25]
 
 
-# def some(x):
-#     return x/5
-
-# var = lambda x: x/5
-
-# print(some(7)) # 1.4
-# print(var(7)) # 1.4
+# Для сравнения, рассмотрим использование обычной функции и lambda-функции:
+def divide_by_five(x):
+    return x / 5
 
 
-# from dis import dis
+lambda_divide_by_five = lambda x: x / 5
+print(divide_by_five(7))  # Вывод: 1.4
+print(lambda_divide_by_five(7))  # Вывод: 1.4
 
+# Lambda-функции часто используются для указания ключа сортировки:
+list_of_people = [["Adam", 29], ["Jonny", 1 / 12], ["Jess", 25]]
 
-# def some(x):
-#     return x / 5
+# Сортировка списка списков по второму элементу каждого подсписка:
+sorted_list = sorted(list_of_people, key=lambda x: x[1])
+print(sorted_list)  # Вывод: [['Jonny', 0.08333333333333333], ['Jess', 25], ['Adam', 29]]
 
+# Фильтрация списка с использованием lambda-функции:
+adults = list(filter(lambda person: person[1] >= 18, list_of_people))
+print(adults)  # Вывод: [['Adam', 29], ['Jess', 25]]
 
-# var = lambda x: x / 5
-# print(dis(some))
-# print(dis(var))
-
-
-# list_of = [["Adam", 29], ["Jonny", 1 * 1 / 12], ['Jess', 25]]
-
-# def s(x):
-#     return x[1]
-
-# r = sorted(list_of, key=s)
-# print(r) # [['Jonny', 0.08333333333333333], ['Jess', 25], ['Adam', 29]]
-
-
-# list_of = [["Adam", 29], ["Jonny", 1 * 1 / 12], ["Jess", 25]]
-
-
-# r = sorted(list_of, key=lambda x: x[1])
-# print(r) # [['Jonny', 0.08333333333333333], ['Jess', 25], ['Adam', 29]]
-
-
-list_of = [["Adam", 29], ["Jonny", 1 * 1 / 12], ["Jess", 25]]
-
-x = list(filter(lambda x: x[1] > 18, list_of))
-
-print(x) # [['Adam', 29], ['Jess', 25]]
+# Lambda-функции являются полезным инструментом для создания коротких функций на лету и часто используются
+# там, где функциональность требуется однократно или в небольшом объеме кода.
