e0b8bce2b612951081ff0b57d28a02957f0eab24
Sarmat <Spirator@Yandex.ru>
2024-04-16 17:38:05 +0700

    Внесены небольшие изменения

--- Lesson_7_Цикл_while.py
+++ Lesson_7_Цикл_while.py
@@ -7,11 +7,11 @@ import webbrowser
 print("Введите 'Завершить' для выхода из программы.")
 while True:
     website = input("Введите адрес сайта: ")
-    if website.lower() == "завершить":  # .lower() позволяет принимать "Завершить" в любом регистре
+    if website.lower() == "завершить" or website == "":  # .lower() позволяет принимать "Завершить" в любом регистре
         break
     if "https://" not in website:
         if "www." not in website:
             website = f"https://www.{website}"  # Предполагаем, что адрес должен начинаться с https://www.
         else:
             website = f"https://{website}"  # Добавляем только префикс https://
-    webbrowser.open(website)
+    webbrowser.open_new(website)  # Открываем сайт в новом окне браузера
